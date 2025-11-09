b622bce1e4c4f6626fc21979288b819353e41234
MaksimMaksimchyk <maksim.maksimchyk@yandex.by>
2025-11-09 19:34:21 +0300

    Ex1 and Ex2 done

--- app/src/main/java/com/example/homework_20/MainActivity.kt
+++ app/src/main/java/com/example/homework_20/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.homework_20
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.homework_20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var itemsCount = 0
+        val listOfItems: MutableList<String> = mutableListOf()
+        binding.recycler.adapter = MyAdapter(listOfItems)
+        binding.recycler.layoutManager = LinearLayoutManager(this)
+
+        binding.floatingAddButton.setOnClickListener {
+            itemsCount++
+            listOfItems.add(itemsCount.toString())
+            binding.recycler.adapter?.notifyItemInserted(itemsCount)
+            binding.textNullList.visibility = TextView.INVISIBLE
+        }
+
     }
 }
